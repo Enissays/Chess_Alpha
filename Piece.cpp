91870d0dfb6e0bba0cfa9ec9638b5d94b826baee
== <tahabarghouti.01@gmail.com>
2023-03-08 16:15:57 +0100

    Co-authored-by: Enissays <Enissays@users.noreply.github.com>

--- Piece.cpp
+++ Piece.cpp
@@ -25,114 +25,133 @@ void Piece::setPos(Pos pos)
     this->pos = pos;
 }
 
-std::vector <Pos> Piece::getMoves(Board board)
+void Piece::addPawnMoves(Pos mv, Board board)
+{
+    // check if there's at least a piece in front of the pawn
+    if (board.table[pos.x][pos.y + (1*board.turn)] == 0) {
+        // check if the pawn is at the starting position
+        if (pos.y == 1 || pos.y == 6) {
+            // check if there's a piece 2 squares in front of the pawn
+            if (board.table[pos.x][pos.y + (2*board.turn)] == 0) {
+                moves.push_back({pos.x, pos.y + (2*board.turn)});
+            }
+        }
+        moves.push_back({pos.x, pos.y + (1*board.turn)});
+    }
+}
+
+void Piece::addRookMoves(Pos mv, Board board)
+{
+        // Rook
+
+    for (int i = pos.y; i < 8; i++) {
+        Pos move1 = {pos.x, i};
+        if (board.table[move1.x][move1.y] == 0){
+            moves.push_back(move1);
+        } else break;
+    }
+
+    for (int i = pos.x; i < 8; i++) {
+        Pos move1 = {i, pos.y};
+        if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
+            moves.push_back(move1);
+        } else break;
+    }
+
+    for (int i = pos.x; i >= 0 ; i--) {
+        Pos move1 = {i, pos.y};
+        if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
+            moves.push_back(move1);
+        } else break;
+    }
+
+    for (int i = pos.y; i >= 0 ; i--) {
+        Pos move1 = {pos.x, i};
+        if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
+            moves.push_back(move1);
+        } else break;
+    }
+            
+}
+
+void Piece::addKingMoves(Pos pos, Board board)
+{
+    moves.push_back({pos.x + 1, pos.y + 2});
+    moves.push_back({pos.x + 1, pos.y - 2});
+    moves.push_back({pos.x - 1, pos.y + 2});
+    moves.push_back({pos.x - 1, pos.y - 2});
+    moves.push_back({pos.x + 2, pos.y + 1});
+    moves.push_back({pos.x + 2, pos.y - 1});
+    moves.push_back({pos.x - 2, pos.y + 1});
+    moves.push_back({pos.x - 2, pos.y - 1});
+
+    // remove the moves that are out of the board and the moves that are blocked by other pieces
+    for (int i = 0; i < moves.size(); i++) {
+    if (moves[i].x < 0 || moves[i].x > 7 || moves[i].y < 0 || moves[i].y > 7) 
+    {
+            moves.erase(moves.begin() + i);
+            i--;
+        } else if (board.table[moves[i].x][moves[i].y] != 0) {
+            moves.erase(moves.begin() + i);
+            i--;
+        }
+    }
+}
+
+void Piece::addBishopMoves(Pos pos, Board board)
+{
+    for (int i = 0; i < 8; i++) {
+        Pos move1 = {pos.x + i, pos.y + i};
+        if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
+            moves.push_back(move1);
+        } else break;
+    }
+
+    for (int i = 0; i < 8; i++) {
+        Pos move1 = {pos.x + i, pos.y - i};
+        if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
+            moves.push_back(move1);
+        } else break;
+    }
+
+    for (int i = 0; i < 8; i++) {
+        Pos move1 = {pos.x - i, pos.y + i};
+        if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
+            moves.push_back(move1);
+        } else break;
+    }
+
+    for (int i = 0; i < 8; i++) {
+        Pos move1 = {pos.x - i, pos.y - i};
+        if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
+            moves.push_back(move1);
+        } else break;
+    }
+}
+
+void Piece::getMoves(Board board)
 {
     std::vector <Pos> moves;
     switch(id) {
         case 1:
-            // Pawn
-            // Si le pion est sur la ligne 2 ou 7, il peut avancer de 2 cases
-            if (pos.y == 1 || pos.y == 6) {
-                Pos move1 = {pos.x, pos.y + 1*board.turn};
-                Pos move2 = {pos.x, pos.y + 2*board.turn}; 
-                // Vérifie si les cases sont vides
-                if (board.table[move1.x][move1.y] == 0) {
-                    moves.push_back(move1);
-                    moves.push_back(move2);
-                }
-            } else {
-                // Sinon, il ne peut avancer que d'une case
-                Pos move1 = {pos.x, pos.y + 1*board.turn};
-                // Vérifie si la case est vide
-                if (board.table[move1.x][move1.y] == 0 ) moves.push_back(move1);
-            }
-            break;
+            addPawnMoves(pos, board);
+        break;
         case 2:
             // Rook
-            for (int i = pos.y; i < 8; i++) {
-                Pos move1 = {pos.x, i};
-                if (board.table[move1.x][move1.y] == 0) {
-                    moves.push_back(move1);
-                } else break;
-            }
-
-            for (int i = pos.x; i < 8; i++) {
-                Pos move1 = {i, pos.y};
-                if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
-                    moves.push_back(move1);
-                } else break;
-            }
-
-            for (int i = pos.x; i >= 0 ; i--) {
-                Pos move1 = {i, pos.y};
-                if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
-                    moves.push_back(move1);
-                } else break;
-            }
-
-            for (int i = pos.y; i >= 0 ; i--) {
-                Pos move1 = {pos.x, i};
-                if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
-                    moves.push_back(move1);
-                } else break;
-            }
-            
-            break;
+            addRookMoves(pos, board);
+        break;
         case 3:
             // Knight
-            moves.push_back({pos.x + 1, pos.y + 2});
-            moves.push_back({pos.x + 1, pos.y - 2});
-            moves.push_back({pos.x - 1, pos.y + 2});
-            moves.push_back({pos.x - 1, pos.y - 2});
-            moves.push_back({pos.x + 2, pos.y + 1});
-            moves.push_back({pos.x + 2, pos.y - 1});
-            moves.push_back({pos.x - 2, pos.y + 1});
-            moves.push_back({pos.x - 2, pos.y - 1});
-
-            // remove the moves that are out of the board and the moves that are blocked by other pieces
-            for (int i = 0; i < moves.size(); i++) {
-                if (moves[i].x < 0 || moves[i].x > 7 || moves[i].y < 0 || moves[i].y > 7) {
-                    moves.erase(moves.begin() + i);
-                    i--;
-                } else if (board.table[moves[i].x][moves[i].y] != 0) {
-                    moves.erase(moves.begin() + i);
-                    i--;
-                }
-            }
-
+            addKnightMoves(pos, board);
             break;
         case 4:
             // Bishop
-            for (int i = 0; i < 8; i++) {
-                Pos move1 = {pos.x + i, pos.y + i};
-                if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
-                    moves.push_back(move1);
-                } else break;
-            }
-
-            for (int i = 0; i < 8; i++) {
-                Pos move1 = {pos.x + i, pos.y - i};
-                if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
-                    moves.push_back(move1);
-                } else break;
-            }
-
-            for (int i = 0; i < 8; i++) {
-                Pos move1 = {pos.x - i, pos.y + i};
-                if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
-                    moves.push_back(move1);
-                } else break;
-            }
-
-            for (int i = 0; i < 8; i++) {
-                Pos move1 = {pos.x - i, pos.y - i};
-                if (board.table[move1.x][move1.y] == 0 && move1.x <= 7 && move1.x >= 0 && move1.y <= 7 && move1.y >= 0) {
-                    moves.push_back(move1);
-                } else break;
-            }
+            addBishopMoves(pos, board);
             break;
         case 5:
             // Queen
+            addRookMoves(pos, board);
+            addBishopMoves(pos, board);
 
             break;
         case 6:
@@ -142,12 +161,12 @@ std::vector <Pos> Piece::getMoves(Board board)
         default:
             break;
     }
-    return moves;
+    this->moves = moves;
 }
 
 bool Piece::checkMove(Pos move, Board board)
 {
-    std::vector <Pos> moves = getMoves(board);
+    getMoves(board);
     for (int i = 0; i < moves.size(); i++) {
         if (moves[i].x == move.x && moves[i].y == move.y) return true;
     }
--- Piece.h
+++ Piece.h
@@ -18,11 +18,22 @@ public:
     ~Piece();
     Pos getPos();
     void setPos(Pos);
-    std::vector <Pos> getMoves(Board);
     bool checkMove(Pos, Board);
+
+    // Function for each type of piece that adds the possible moves to the vector
+    void addPawnMoves(Pos, Board);
+    void addRookMoves(Pos, Board);
+    void addKnightMoves(Pos, Board);
+    void addBishopMoves(Pos, Board);
+    void addQueenMoves(Pos, Board);
+    void addKingMoves(Pos, Board);
+    void getMoves(Board);
+
 private:
     Pos pos;
     int id;
+    std::vector <Pos> moves;
+    std::vector <Pos> kills;
 
 };
 
